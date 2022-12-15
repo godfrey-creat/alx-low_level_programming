fe232ac78d1d477ec674dbb50aabeecd5048d842
godfrey-creat <ogodfrey135@gmail.com>
2022-12-15 02:50:47 -0800

    positive or negative

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -12,19 +12,19 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(45);
+		_putchar(43);
 
 		return (1);
 	}
 	else if (n < 0)
 	{
-		_putchar(48);
+		_putchar(45);
 
 		return (-1);
 	}
 	else
 	{
-		_putchar(51);
+		_putchar(48);
 
 		return (-0);
 	}
