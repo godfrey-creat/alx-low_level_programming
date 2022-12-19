af02c6470cedbb0e9486139d255562f01cb5d845
godfrey-creat <ogodfrey135@gmail.com>
2022-12-19 06:38:47 -0800

    half reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,19 @@
 
 void rev_string(char *s)
 {
-	int m, n;
+	int m, n, half;
 	char ch;
 
 	for (m = 0; s[m] != '\0'; ++m)
 		;
-	for (n = 0; n < 1 / 2; ++n)
+	n = 0;
+	half = m / 2;
+
+	while (half--)
 	{
-		ch = s[n];
+		ch = s[m - n - 1];
 		s[n] = s[m - 1 - n]; /* -1 because array starts from 0*/
-		s[m - 1 - n] = ch;
+		s[n] = ch;
+		n++;
 	}
 }
