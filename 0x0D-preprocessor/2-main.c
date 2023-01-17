3e9952750367b04f858c3aa7a6129ac342c494bc
godfrey-creat <ogodfrey135@gmail.com>
2023-01-17 12:04:25 -0800

    name of the program

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - program that prints the name of the file compilation starts from
+ * @description: name of the file it was compiled from
+ * @author: godfrey-creat
+ * Return: 0
+ */
+int main(void)
+{
+	printf("%s\n", FILE);
+	return (0);
+}
