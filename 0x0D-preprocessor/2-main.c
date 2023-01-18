acabd57f3f3b9b1291dc493cc263a54796eddf5e
godfrey-creat <ogodfrey135@gmail.com>
2023-01-18 11:28:23 -0800

    name of the file

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - program that prints the name of the file compilation starts from
- * FILE: name of the file
+ * main - program that prints the name of the file
  * Return: 0
  */
 int main(void)
 {
-	printf("%s\n", (FILE));
+	printf("%s\n", __FILE__);
 	return (0);
 }
