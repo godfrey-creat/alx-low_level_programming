33cd15dddc826352562723263c8a4faecbd6b017
godfrey-creat <ogodfrey135@gmail.com>
2023-01-31 01:18:49 -0800

    free a linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * free_list - free single list link
+ * @head: pointer to the first node
+ * Return: norhing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	/**
+	 * while current head is not null set headas next node
+	 * and free the current node str then
+	 * free current node
+	 */
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
