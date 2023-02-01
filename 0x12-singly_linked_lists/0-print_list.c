50d010b51f266b59eb17e2930a803d2e711fe6fa
godfrey-creat <ogodfrey135@gmail.com>
2023-02-01 00:07:41 -0800

    printing list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,23 +7,16 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t node_count = 1;
-	/*return 0 as number of nodes*/
-	if (h == NULL)
-		return (0);
-	while (h->next != NULL)
+	unsigned int count = 0;
+
+	while (h != NULL)
 	{
-		/*if str is NULL return len as 0 and str as (nil)*/
 		if (h->str == NULL)
-			printf("[%d]%s\n", 0, "(nil)");
+			printf("[0] (nil)\n");
 		else
-			printf("[%d]%s\n", h->len, h->str);
-		/*proceed to next node*/
+			printf("[%d %s\n", h->len, h->str);
 		h = h->next;
-		/*counting number of nodes*/
-		node_count += 1;
+		count++;
 	}
-	/*printing the last node*/
-	printf("[%d]%s\n", h->len, h->str);
-	return (node_count);
+	return (count);
 }
